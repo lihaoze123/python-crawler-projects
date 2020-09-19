3e5990d233b0b23940ac62b5e9c6d3a2837a5a8f
lihaoze123 <2595248810@qq.com>
2020-09-20 02:09:44 +0800

    更新‘search_file.py’

---
+++ Projects/Search_file/search_file.py
@@ -0,0 +1,50 @@
+import os, re, pathlib
+
+def get_dir(dirname = str(pathlib.Path.home())):
+    if os.name == "posix":
+        os.system("clear")
+    elif os.name == "nt":
+        os.system("cls")
+
+    exit = False
+    list_dirs = list()
+    content = list()
+    dir_dict = {}
+
+    step = 0
+
+    for i in os.listdir(dirname):
+        if os.path.isdir(dirname + "/" + i):
+            step += 1
+            dir_dict[step] = i
+            content.append("{num}   {dir}\n".format(num = step, dir = i))
+        content_result = "".join(content)
+
+    selected = input("已选择的文件夹'{selected}'\n请输入要选择的文件夹(按q退出):\n{content}".format(selected = dirname,content = content_result))
+
+    if selected == "q":
+        return 0
+    else:
+        result = dirname + "/" + dir_dict[int(selected)]
+        get_dir(result)
+        return result
+
+
+
+def findfile(root_dir, file_type):
+    list_dir = os.listdir(root_dir)
+    list_file = list()
+    compile = re.compile(r"^(.*?)..{}$".format(file_type))
+    for dir in list_dir:
+        dir = "{root_dir}/{dir}".format(root_dir=root_dir, dir=dir)
+        if os.path.isdir(dir):
+            list_file = list_file + findfile(dir, file_type)
+        elif re.findall(compile, dir):
+            list_file.append(dir)
+    return list_file
+
+
+if __name__ == '__main__':
+    dir = get_dir()
+    find = input("请输入要查找的文件扩展名:\n")
+    print(findfile(dir, find))
